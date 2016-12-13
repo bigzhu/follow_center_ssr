1108f11f14c7a67dd82322ff6ad2ad84aebb15cb
bigzhu <vermiliondun@gmail.com>
2016-12-13 14:52:50 +0800

    Main可以过来

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,24 +8,7 @@
         <div class="five wide column computer only ">
           <div class="blank-bz"></div>
           <god-info v-show="god_name" :god_info="god_info"></god-info>
-          <div class="ui card border-radius">
-            <div class="content">
-              <div class="description align-center">
-                ￥10 微信扫一扫，<i class="coffee icon"></i>买杯咖啡赞助我
-              </div>
-            </div>
-            <div class="ui small centered image">
-              <img src="../../static/assets/by_me.jpg">
-            </div>
-            <div class="content">
-              <div class="description align-center">
-                <i class="qq icon"></i>答疑QQ群:&nbsp&nbsp484391016
-              </div>
-            </div>
-          </div>
-          <div v-show="registered_count !== -1"class="description sign-imfor align-center">
-            <i class="user icon"></i>注册人数：{{registered_count}}
-          </div>
+          <right-info></right-info>
         </div>
       </div>
     </div>
@@ -35,19 +18,20 @@
 </template>
 
 <script>
-  import store from '../store'
   import '../assets/mobile.css'
   import $ from 'jquery'
   import Messages from './Messages'
   import Top from './Top'
   import UnRead from './UnRead'
   import GodInfo from './GodInfo'
+  import RightInfo from './RightInfo'
   export default {
     components: {
       UnRead,
       Top,
       Messages,
-      GodInfo
+      GodInfo,
+      RightInfo
     },
     data () {
       return {
@@ -61,15 +45,11 @@
           return god_info
         }
       },
-      registered_count () {
-        return this.$store.state.registered_count
-      },
       god_name () {
         if (this.$route.params.god_name) return this.$route.params.god_name.toLowerCase()
       }
     },
     mounted () {
-      this.$store.dispatch('getRegisteredCount')
       if (this.god_name) { this.$store.dispatch('getGod', this.god_name) }
       $('body').visibility()
     },
@@ -92,7 +72,7 @@
     beforeRouteLeave (to, from, next) {
       $(window).off('scroll')
       $(window).unbind('scroll')
-      store.commit('SET_SHOW_BAR', true) // 离开时，确保Bar显示出来
+      this.$store.commit('SET_SHOW_BAR', true) // 离开时，确保Bar显示出来
       next()
     }
   }
@@ -106,7 +86,7 @@
     padding: 0rem!important;
   }
   .blank-bz {
-    padding-top: 35px;
+    padding-top: 49px;
   }
   .ui.card.border-radius {
     border-radius: 0.06em;
@@ -123,6 +103,36 @@
   .sign-imfor {
     color: #9A9A9A;
     font-size: 1em;
+  }
+  .footer-content a {
+    color: #9A9A9A;
+    font-size: 1em;
+  }
+  .footer-content a:hover {
+    color: #515151;
+    transition: color 0.3s ease;
+    transition: visibility 0s, opacity 0.3s linear;
+  }
+  .footer-content {
+    color: #9A9A9A;
+    margin-top: 30px;
+    padding: 15px 15px;
+    width: auto;
+    font-size: 1rem;
+    border-top: 1px solid #eee;
+    line-height: 2rem;
+  }
+  .no-message {
+    color: #9A9A9A;
+    margin-top: 30px;
+    padding: 15px 15px;
+    width: auto;
+    font-size: 1rem;
+    line-height: 2rem;
+    text-align: center;
+  }
+  .no-message p {
+    margin-top: 1rem;
   }
   @media only screen and (max-width: 767px){
     .ui.container {
